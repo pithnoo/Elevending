a417e29bdaa7c43dd6d035a82069fbcae38cf116
pith <ollyc88@gmail.com>
2023-01-26 21:23:56 +0000

    added player animations and type damage

--- Code/Enemy/enemy_hurt_box.gd
+++ Code/Enemy/enemy_hurt_box.gd
@@ -4,9 +4,7 @@ export(NodePath) var entity_stats
 
 onready var stats = get_node(entity_stats)
 
-signal same_type
-signal adv_type
-signal disadv_type
+signal buff_enemy 
 
 enum damage_type { GRASS, WATER, FIRE }
 export(damage_type) var type
@@ -17,38 +15,28 @@ func _on_HurtBox_area_entered(area:Area2D):
       # type matchups for grass
       match area.type:
         area.element.GRASS:
-          pass
+          emit_signal("buff_enemy")
         area.element.WATER:
-          pass
+          stats.health -= area.damage / 2
         area.element.FIRE:
-          pass
+          stats.health -= area.damage
     damage_type.WATER:
       # type matchups for water
       match area.type:
         area.element.GRASS:
-          pass
+          stats.health -= area.damage
         area.element.WATER:
-          pass
+          emit_signal("buff_enemy")
         area.element.FIRE:
-          pass
+          stats.health -= area.damage / 2
     damage_type.FIRE:
-      # type matchups for grass
+      # type matchups for fire 
       match area.type:
         area.element.GRASS:
-          pass
+          stats.health -= area.damage / 2
         area.element.WATER:
-          pass
+          stats.health -= area.damage
         area.element.FIRE:
-          pass
+          emit_signal("buff_enemy")
 
-  stats.health -= area.damage
   area.destroy()
-
-func damage_enemy():
-  emit_signal("adv_type")
-
-func buff_enemy():
-  emit_signal("same_type")
-
-func half_damage_enemy():
-  emit_signal("disadv_type")
