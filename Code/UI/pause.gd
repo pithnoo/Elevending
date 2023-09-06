9bc6c04f400d4c2fb8c5d044a341714c57fa476e
pithnoo <ollyc88@gmail.com>
2023-09-07 00:54:31 +0100

    added scene transition to menu

--- Code/UI/pause.gd
+++ Code/UI/pause.gd
@@ -1,7 +1,22 @@
 extends Control
 
+export(String, FILE, "*.tscn,*.scn") var menu
+
 func _input(event):
 	if event.is_action_pressed("pause"):
 		var current_pause_state = not get_tree().paused
 		get_tree().paused = current_pause_state
 		visible = current_pause_state
+
+func _on_MenuButton_pressed():
+	SceneTransition.blind_transition(menu)
+	#SceneTransition.upgrade_transition(menu)
+	#SceneTransition.return_transition(menu)
+
+func _on_SettingsButton_pressed():
+	pass # Replace with function body.
+
+func _on_ResumeButton_pressed():
+	var current_pause_state = not get_tree().paused
+	get_tree().paused = current_pause_state
+	visible = current_pause_state
--- Code/UI/scene_transition.gd
+++ Code/UI/scene_transition.gd
@@ -1,16 +1,36 @@
-extends Node
+extends CanvasLayer
 
+onready var animations = $AnimationPlayer
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func change_scene(transition_target : String, transition_type : int):
+	match transition_type:
+		1:
+			# blinds transition
+			blind_transition(transition_target)
+		2:
+			# upgrade transition
+			upgrade_transition(transition_target)
+		3:
+			# return to return transition
+			return_transition(transition_target)
+		_:
+			# in case invalid value is entered
+			print("transition does not exist")
 
+func blind_transition(target : String) -> void:
+	animations.play("BlindsTransition")
+	yield(animations, "animation_finished")
+	get_tree().change_scene(target)
+	animations.play_backwards("BlindsTransition")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func upgrade_transition(target : String) -> void:
+	animations.play("UpgradeEnter")
+	yield(animations, "animation_finished")
+	get_tree().change_scene(target)
+	animations.play("UpgradeExit")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func return_transition(target : String) -> void:
+	animations.play("ReturnEnter")
+	yield(animations, "animation_finished")
+	get_tree().change_scene(target)
+	animations.play("ReturnExit")
